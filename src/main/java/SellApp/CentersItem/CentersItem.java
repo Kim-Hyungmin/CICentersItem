a6acf561cc28abbe27035bc2656ef4b3c23f9f0c
Kim-Hyungmin <khmin6998@gmail.com>
2020-12-09 16:36:51 +0900

    final?

---
+++ src/main/java/SellApp/CentersItem/CentersItem.java
@@ -0,0 +1,31 @@
+package SellApp.CentersItem;
+
+/**
+ * Hello world!
+ *
+ */
+public class CentersItem 
+{
+	 private int map; // 프로필사진
+	    private String info; // 상담 센터 정보
+	    private String phone; // 연락처
+
+	    public int getMap() {
+	        return map;
+	    }
+
+	    public String getInfo() {
+	        return info;
+	    }
+
+	    public String getPhone() {
+	        return phone;
+	    }
+
+	    public CentersItem(int map, String info, String phone) {
+	        this.map = map;
+	        this.info = info;
+	        this.phone = phone;
+
+	    }
+}
---
+++ src/test/java/SellApp/CentersItem/CentersItemTest.java
@@ -0,0 +1,20 @@
+package SellApp.CentersItem;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class CentersItemTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+	public void testCentersItem() {
+		CentersItem c = new CentersItem(0, null, null);
+		assertNotNull(c);
+	}
+}
